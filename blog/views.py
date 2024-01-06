83a5192f3eaa9d9a50ac489ca1de734493b5c6c3
Anthony Frank A. Ordonez <anthonyfrankaordonez@gmail.com>
2024-01-06 17:55:10 +0800

    Added some comments or docstrings in every and each of the function in views

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,21 @@ from django.utils import timezone
 from .forms import PostForm
 
 def post_list(request):
+    """View function to view all the blog post in the website"""
+
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
 def post_detail(request, pk):
+    """View function to view the details of the blog post"""
+
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
 def post_new(request):
+    """View function to be able to add new blog post"""
 
     if request.method == 'POST':
         form = PostForm(request.POST)
@@ -33,6 +38,8 @@ def post_new(request):
 
 
 def post_edit(request, pk):
+    """View function to be able to edit existing blog post"""
+
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == 'POST':
