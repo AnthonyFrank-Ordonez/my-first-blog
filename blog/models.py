a77fa8940fa38c2b8b792401d6bf31543fe1afac
Anthony Frank A. Ordonez <anthonyfrankaordonez@gmail.com>
2024-01-08 17:40:37 +0800

    Added some view function to be able to view draft, publish as well as add comments and remove comments from the blog post

--- blog/models.py
+++ blog/models.py
@@ -16,5 +16,24 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comments=True)
+
     def __str__(self):
         return self.title
+    
+class Comment(models.Model):
+    """Comments Model"""
+
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comments = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comments = True
+        self.save()
+    
+    def __str__(self):
+        return self.text
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post
+from .models import Post, Comment
 from django.utils import timezone
-from .forms import PostForm
+from .forms import PostForm, CommentForm
+from django.contrib.auth.decorators import login_required
 
+
+@login_required
 def post_list(request):
     """View function to view all the blog post in the website"""
 
@@ -10,6 +13,7 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 
+@login_required
 def post_detail(request, pk):
     """View function to view the details of the blog post"""
 
@@ -17,6 +21,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
+@login_required
 def post_new(request):
     """View function to be able to add new blog post"""
 
@@ -26,7 +31,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -37,6 +41,7 @@ def post_new(request):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 
+@login_required
 def post_edit(request, pk):
     """View function to be able to edit existing blog post"""
 
@@ -48,7 +53,6 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
 
             return redirect('post_detail', pk=post.pk)
@@ -56,3 +60,63 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
 
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@login_required
+def post_draft_list(request):
+    """View function to be able to see all the drafts bllog post"""
+
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+
+@login_required
+def post_publish(request, pk):
+    """View Function to be able to publish a blog post"""
+
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def post_remove(request, pk):
+    """View Function to be able to remove a blog post"""
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
+
+def add_comment_to_post(request, pk):
+    """View Function to add comment to the post"""
+    post = get_object_or_404(Post, pk=pk)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    
+    return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    """View function to approved a comment and to display it to the number of comments"""
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    """View function to be able to remove and disapproved a comment"""
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return redirect('post_detail', pk=comment.post.pk)
